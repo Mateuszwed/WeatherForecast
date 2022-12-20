6ebee6e9fd6a771c418f92b99a2edea0912c2363
Mateusz Szwed <mateusz.szwed.programista@gmail.com>
2022-12-20 12:48:58 +0100

    Created baseController and MainWindow controller, added styles css

--- src/main/java/com/example/weatherforecast/Launcher.java
+++ src/main/java/com/example/weatherforecast/Launcher.java
@@ -1,5 +1,6 @@
 package com.example.weatherforecast;
 
+import com.example.weatherforecast.view.ViewFactory;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,6 +13,7 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        ViewFactory viewFactory = new ViewFactory();
+        viewFactory.showMainWindow();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,13 @@ module com.example.weatherforecast {
     requires javafx.controls;
     requires javafx.fxml;
 
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.weatherforecast to javafx.fxml;
+    opens com.example.weatherforecast.controller to javafx.fxml;
     exports com.example.weatherforecast;
+    opens com.example.weatherforecast.view to javafx.fxml;
 }
