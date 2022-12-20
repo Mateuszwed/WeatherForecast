9b3fb0d1190f5264fc1010b00139c3afe7cac65d
Mateusz Szwed <mateusz.szwed.programista@gmail.com>
2022-12-20 13:02:14 +0100

    Added controlls

--- src/main/java/com/example/weatherforecast/controller/MainWindowController.java
+++ src/main/java/com/example/weatherforecast/controller/MainWindowController.java
@@ -1,19 +1,104 @@
 package com.example.weatherforecast.controller;
 
 import com.example.weatherforecast.view.ViewFactory;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainWindowController extends BaseController implements Initializable {
 
-    public MainWindowController(ViewFactory viewFactory, String fxmlName) {
-        super(viewFactory, fxmlName);
+    @FXML
+    private TextField leftCountryTextField;
+
+    @FXML
+    private TextField leftCityTextField;
+
+    @FXML
+    private Label countryAndCityLabel;
+
+    @FXML
+    private HBox leftWeatherBox;
+
+    @FXML
+    private ImageView leftIcon;
+
+    @FXML
+    private Label leftTemperatureLabelShow;
+
+    @FXML
+    private Label leftWindLabelShow;
+
+    @FXML
+    private Label leftPressureLabelShow;
+
+    @FXML
+    private Label leftHumidityLabelShow;
+
+    @FXML
+    private HBox leftExtendedForecast;
+
+    @FXML
+    private Label leftError;
+
+    @FXML
+    private TextField rightCountryTextField;
+
+    @FXML
+    private TextField rightCityTextField;
+
+    @FXML
+    private Label rightCountryAndCityLabel;
+
+    @FXML
+    private HBox rightWeatherBox;
+
+    @FXML
+    private ImageView rightIcon;
+
+    @FXML
+    private Label rightTemperatureLabelShow;
+
+    @FXML
+    private Label rightWindLabelShow;
+
+    @FXML
+    private Label rightPressureLabelShow;
+
+    @FXML
+    private Label rightHumidityLabelShow;
+
+    @FXML
+    private HBox rightExtendedForecast;
+
+    @FXML
+    private Label rightError;
+
+    @FXML
+    void showLeftWeatherButtonAction() {
+
+    }
+
+    @FXML
+    void showRightWeatherButtonAction() {
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        leftWeatherBox.setVisible(false);
+        rightWeatherBox.setVisible(false);
+
     }
+
+    public MainWindowController(ViewFactory viewFactory, String fxmlName) {
+        super(viewFactory, fxmlName);
+    }
+
 }
